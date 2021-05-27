2be2ae6be7248790ce5e62767d2dc5be9665ef2d
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-27 02:31:03 +0000

    AssertionError

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# lp3thw p. 142
+# a = 1
+
 try:
-  print("a =", a)
+  assert False, "(코드 짠 사람 속이 뒤틀렸는지) 무조건 예외 발생~~"
+  print("a is", a)
 except NameError as e:
   print(e)
+except AssertionError as f:
+  print("예외 발생:", f)
+  # raise f
 finally:
   print("we may omit 'finally'")
 
