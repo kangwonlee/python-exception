429cd23b263922a32f272c80a06682d0b7699821
KangWon LEE <kangwon.lee.edu@gmail.com>
2021-05-28 05:53:16 +0000

    three try-except blocks

--- main.py
+++ main.py
@@ -2,14 +2,46 @@
 # a = 1
 
 try:
-  assert False, "(코드 짠 사람 속이 뒤틀렸는지) 무조건 예외 발생~~"
   print("a is", a)
+  assert False, "무조건 예외 발생"
 except NameError as e:
   print(e)
 except AssertionError as f:
   print("예외 발생:", f)
-  # raise f
 finally:
   print("we may omit 'finally'")
 
-print("after the try except block")
+print("after the first try except block")
+
+
+print('=' * 10)
+a = 1
+
+try:
+  print("a is", a)
+  assert False, "무조건 예외 발생"
+except NameError as e:
+  print(e)
+except AssertionError as f:
+  print("예외 발생:", f)
+finally:
+  print("we may omit 'finally'")
+
+print("after the second try except block")
+
+
+print('=' * 10)
+a = 2
+
+try:
+  print("a is", a)
+  assert False, "무조건 예외 발생"
+except NameError as e:
+  print(e)
+except AssertionError as f:
+  print("예외 발생:", f)
+  raise f
+finally:
+  print("we may omit 'finally'")
+
+print("after the third try except block")
